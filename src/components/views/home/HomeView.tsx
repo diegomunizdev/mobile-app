514d10983e6337f67ca2e11fb2748ae60d494fb5
diegomunizdev <diegomunizdev@outlook.com>
2023-11-14 10:18:07 -0300

    main: remove needs

--- src/components/views/home/HomeView.tsx
+++ src/components/views/home/HomeView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
 import { Text, View } from 'react-native';
 
 export default function HomeView() {
